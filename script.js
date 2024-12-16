de39011d64e2875f19b371785aa9338be512cf9b
talha-hyphen <w00052207@gmail.com>
2024-12-16 10:52:44 -0800

    assignment 8

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+//2. Write a js program to find maximum between three numbers.
+//var number1 = prompt("Enter 1st Number")
+//var number2 = prompt("Enter 2nd Number")
+//var number3 = prompt("Enter 3rd Number")
+//if (number1>number2 && number1>number3) {
+ //   alert("1st Number is Greater")
+//}
+//else if (number1<number2 && number2>number3) {
+  //  alert("2nd Number is Greater")
+//}else if (number1<number3 && number2<number3) {
+ //   alert("3rd Number is Greater")
+//}
+//else if (number2==number1==number3){
+ //   alert("1st number = 2nd number = 3rd number  ")
+//}
+
+
+
+
+
+//1. Write a js program to find maximum between two numbers.
+//var num1 = prompt ("Enter a 1st Number");
+//var num2 = prompt ("Enter a 2nd Number");
+// if (num1 > num2) {
+  //  alert ("1st number is greater 2nd")
+ //}
+ // else if(num1 < num2) {
+ //   alert ("2nd number is greater 1st")
+ //}
+//else if(num1 == num2) {
+ //   alert ("1st number = 2nd number")
+ //}
+
+
+
+
